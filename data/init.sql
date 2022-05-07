92fbfb1c939cc090289432443a81338d7806063f
Patryk Sandomierski <psandomierski@sii.pl>
2022-05-07 15:09:57 +0200

    Add grafana dashboards; Add fake data for datasource

--- data/init.sql
+++ data/init.sql
@@ -1,4 +1,27 @@
-CREATE TABLE test (id int, test_message varchar(64));
-INSERT INTO test VALUES (1, 'one');
-INSERT INTO test VALUES (2, 'two');
-INSERT INTO test VALUES (3, 'three');
+CREATE TABLE climate (climate_id SERIAL NOT NULL PRIMARY KEY,
+    measure_timestamp TIMESTAMP,
+    temperature REAL,
+    humidity REAL,
+    pressure REAL
+);
+
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES (current_timestamp - (2 * interval '1 minute'), 23.3333, 35.00000, 1024.3123);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES (current_timestamp - (4 * interval '1 minute'), 22.3333, 32.00000, 1023.3212);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES (current_timestamp - (6 * interval '1 minute'), 21.3333, 31.00000, 1022.3199);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES (current_timestamp - (8 * interval '1 minute'), 20.3333, 34.00000, 1022.3123);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (10 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (12 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (14 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (16 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (18 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (20 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (22 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);
+INSERT INTO climate (measure_timestamp, temperature, humidity, pressure) VALUES
+    (current_timestamp - (24 * interval '1 minute'), RANDOM() * 10 + 15, RANDOM() *60 + 20, RANDOM() * 10 + 1015);

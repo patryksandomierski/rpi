27313f6b483a2104d2cf06e4ff49efb21750d221
Patryk Sandomierski <psandomierski@sii.pl>
2022-05-30 22:25:03 +0200

    Add LCD POC

--- src/sketches/lcd.py
+++ src/sketches/lcd.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python
-import sys
-
 import rgb1602
-import time
 
 lcd = rgb1602.RGB1602(16, 2)  # create LCD object,specify col and row
 
 # Print a message to the LCD.
-lcd.printout("set color")
-print("set color")
-while True:
-    while True:
-        r = int(input("r = "))
-        if r < 256 and r >= 0:
-            break
-        else:
-            print("r is wrong number,input r in 0 ~255")
-    while True:
-        g = int(input("g = "))
-        if g < 256 and g >= 0:
-            break
-        else:
-            print("g is wrong number,input g in 0 ~255")
-    while True:
-        b = int(input("b = "))
-        if b < 256 and b >= 0:
-            break
-        else:
-            print("b is wrong number,input b in 0 ~255")
-    lcd.setRGB(r, g, b)
-    print("get data:r = %s,g = %s,b = %s" % (r, g, b))
+lcd.setRGB(255, 0, 0)
+lcd.setCursor(0, 0)
+lcd.printout("Czesc mama")
+lcd.setCursor(0, 1)
+lcd.printout("Co tam w szkole")

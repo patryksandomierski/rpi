b7a307b102b09f3c4de7327dd34aa1f38a32de17
Patryk Sandomierski <psandomierski@sii.pl>
2022-05-31 20:03:59 +0200

    Add LCD POC

--- src/sketches/buzzer.py
+++ src/sketches/buzzer.py
@@ -2,11 +2,12 @@
 from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 buzzer_id=13
 GPIO.setup(buzzer_id, GPIO.OUT)
 
 while True:
     GPIO.output(buzzer_id, GPIO.HIGH)
-    sleep(1)
+    sleep(0.25)
     GPIO.output(buzzer_id, GPIO.LOW)
     sleep(1)

99c4449be52cf73857b3127d0fc9e553747c7f9a
Patryk Sandomierski <psandomierski@sii.pl>
2022-06-10 18:11:28 +0200

    Add lcd POC

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# rpi - pgsql - grafana = RPG ;)
+# rpi + pgsql + grafana = RPG ;)
 
-## prerequisites on rpi4
+## prerequisites
 
 Used in this project:
 - RPi4 with Raspbian 11 ("heavy" version)
 - bme280
+- DFR0603
 - breadboard and jumper wires (I'm not an electronics engineer... :)
 
 ### docker on rpi4
@@ -24,28 +25,38 @@ sudo pip install docker-compose
 docker-compose --version
 ```
 
-### psycopg2 on rpi4
+### psycopg2 (postgres driver) on rpi4
 
 ```bash
 sudo pip install psycopg2
 sudo apt install python3-psycopg2
 ```
 
-### i2c and bme280 on rpi4
+### i2c on rpi4
 
-Enable i2c interface using cli:
+Enable i2c interface:
 1. `sudo raspi-config`
 2. `Interfacing Options`
 3. `I2C`
 4. ...enabled? `Yes`
 5. ...reboot now? `Yes`
 
-Install bme280 python library: `sudo pip install RPi.bme280`
-If bme280 is connected to rpi, to check proper bus address, use: `i2cdetect -y 1`
+### bme280 (versatile sensor, uses i2c) on rpi4
+
+Install bme280 python library: `sudo pip install RPi.bme280`.
+
+If bme280 is connected to rpi, to check proper bus address, use: `i2cdetect -y $N`.
+`$N` defaults to `1` but since we are using DFR0603, please refer how to set up new i2c bus [here](#multiple-i2c-devices) and get new `$N` :D
+
+### DFR0603 (lcd screen with buttons, uses i2c and gpio) on rpi4
+
+Just install `sudo pip install wiringpi`, device api library is already imported from [here](https://github.com/DFRobot/DFRobot_RGB1602_RaspberryPi).
 
 ## local development
 
-### manjaro - install before other requirements
+### manjaro
+
+install before other requirements
 
 ```bash
 sudo pacman -S postgresql-libs
@@ -53,9 +64,23 @@ sudo pacman -S postgresql-libs
 
 ### trivia
 
+#### running
+
 ```bash
 # run in background even if ssh session is closed
 nohup src/main.py &
 # SIGINT (same as ctrl+c for foreground app) to gracefully close process
 kill -2 $(pgrep main.py)
 ```
+
+#### multiple i2c devices
+
+Create new bus by adding to `/boot/config.txt` e.g. `dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=1,i2c_gpio_sda=23,i2c_gpio_scl=27` for bus 3 and reboot.
+Now some gpio pins will be acting like i2c ones, in this example `gpio-23=sda` and `gpio-27=scl`.
+To validate settings, use `i2cdetect -l` - new bus should be available.
+
+Remember to list buses in reverse order, e.g. 5, 4, 3. Don't use bus 0 and 2.
+
+Pick free GPIO interfaces, e.g. DFR0603 overlay optionally reserves GPIO 16 to 20 for handling buttons.
+
+Source: https://www.instructables.com/Raspberry-PI-Multiple-I2c-Devices/
---
+++ src/sketches/lcd-rotate-msgs.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+import threading
+from collections import deque
+from enum import Enum
+from time import sleep, perf_counter
+
+import bme280
+import smbus2
+import RPi.GPIO as GPIO
+import rgb1602
+import w1thermsensor
+
+# setup bme280
+port = 3
+address = 0x76
+bus = smbus2.SMBus(port)
+bme280.load_calibration_params(bus, address)
+# setup lcd
+lcd = rgb1602.RGB1602(16, 2)
+# setup gpio
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+# setup gpio buttons
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# setup ds18b20
+ds18b20 = w1thermsensor.W1ThermSensor()
+# setup gpio buzzer
+buzz_gpio_out = 13
+GPIO.setup(buzz_gpio_out, GPIO.OUT)
+
+# global sensors values
+temp = 0.0
+humi = 0.0
+press = 0.0
+probe_temp = 0.0
+
+
+class Button(Enum):
+    SELECT = 16
+    UP = 17
+    DOWN = 18
+    LEFT = 19
+    RIGHT = 20
+    NONE = -1
+
+
+def lcd_read_buttons():
+    for btn in Button:
+        if btn != Button.NONE:
+            if GPIO.input(btn.value) == 1:
+                return btn
+    return Button.NONE
+
+
+def lcd_print_messages(messages, indexes=None):
+    if indexes is None:
+        indexes = [0, 1]
+    lcd.setCursor(0, 0)
+    lcd.printout(messages[indexes[0]].ljust(16))
+    lcd.setCursor(0, 1)
+    lcd.printout(messages[indexes[1]].ljust(16))
+
+
+def bme280_read_data_thread():
+    while True:
+        data = bme280.sample(bus, address)
+        global temp, humi, press
+        temp = data.temperature
+        humi = data.humidity
+        press = data.pressure
+        sleep(1)
+
+
+def buzzer_temp_thread():
+    while True:
+        if probe_temp > 25.0:
+            GPIO.output(buzz_gpio_out, GPIO.HIGH)
+            sleep(0.25)
+            GPIO.output(buzz_gpio_out, GPIO.LOW)
+        else:
+            GPIO.output(buzz_gpio_out, GPIO.LOW)
+        sleep(5)
+
+
+def get_sensors_readable_data():
+    return ['Temp.     {}'.format(f"{temp:06.03f}"),
+            'Wilg.     {}'.format(f"{humi:06.03f}"),
+            'Cisn.   {}'.format(f"{press:08.03f}"),
+            'Temp.zewn.{}'.format(f"{probe_temp:06.03f}")]
+
+
+def ds18b20_read_data_thread():
+    while True:
+        global probe_temp
+        probe_temp = ds18b20.get_temperature()
+
+
+def main():
+    threading.Thread(target=ds18b20_read_data_thread).start()
+    threading.Thread(target=bme280_read_data_thread).start()
+    threading.Thread(target=buzzer_temp_thread).start()
+
+    btn_pressed = Button.NONE
+    messages = get_sensors_readable_data()
+    indexes = deque(list(range(len(messages))))
+
+    btn_hold = False
+    time_btn_hold = 0.0
+
+    while True:
+        messages = get_sensors_readable_data()
+        if not btn_hold:
+            lcd_print_messages(messages, indexes)
+        btn_last_state = btn_pressed
+        # noinspection PyUnusedLocal
+        btn_pressed = lcd_read_buttons()
+        sleep(0.1)
+        btn_pressed = lcd_read_buttons()
+        # press
+        if btn_pressed != Button.NONE and btn_pressed != btn_last_state:
+            # scroll
+            if btn_pressed == Button.UP:
+                indexes.rotate(1)
+            elif btn_pressed == Button.DOWN:
+                indexes.rotate(-1)
+        # hold >2.0s
+        elif btn_pressed != Button.NONE and btn_pressed == btn_last_state:
+            if not btn_hold:
+                time_btn_hold = perf_counter()
+            btn_hold = True
+            if perf_counter() - time_btn_hold > 2.0:
+                if btn_pressed == Button.SELECT:
+                    lcd_print_messages(["GG WP", "ciri0x5a"])
+        else:
+            btn_hold = False
+            time_btn_hold = 0.0
+
+
+if __name__ == '__main__':
+    main()
